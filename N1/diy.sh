bc457cd988e1c88e538f1b76634f900edc843c51
woni928 <398401414@qq.com>
2025-02-23 16:26:24 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -7,7 +7,7 @@ sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_gener
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 package/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 package/passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2 --depth=1 package/passwall2
-git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone https://github.com/sbwml/luci-app-alist package/alist
 
 rm -rf feeds/packages/net/v2ray-geodata
